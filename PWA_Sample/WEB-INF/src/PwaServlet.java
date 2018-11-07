b4b27b4ad165f6af80985f749aa9c25bb682fb7b
aa140043 <masuda-kazuya@gr.sei.co.jp>
2018-11-07 14:44:38 +0900

    servlet 追加

---
+++ PWA_Sample/WEB-INF/src/PwaServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PwaServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		System.out.println("doGet");
+	}
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("doPost");
+		res.setContentType("application/json");
+	    res.setCharacterEncoding("utf-8");
+	    res.getWriter().println("{ 'msg': 'hello'}");
+	}
+}
